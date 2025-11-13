9a6ee1833161a804bf5a726111bcb227ea1ee814
clay <clay.sexton@atlasstudents.com>
2025-11-12 22:55:05 -0500

    fixed docs

--- hadoop_project/4-createdir.py
+++ hadoop_project/4-createdir.py
@@ -3,8 +3,8 @@
 from snakebite.client import Client
 
 
-# function to create a directory
 def createdir(l):
+    # function to create a directory
     client = Client('localhost', 9000)
 
     for path in client.mkdir(l, create_parents=True):
